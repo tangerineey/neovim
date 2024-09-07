7b48c000509d697f5e76904874f0357ebd4ede44
tangerineey <107423877+tangerineey@users.noreply.github.com>
2024-09-07 13:08:01 -0600

    feat: remove lazy update notification

--- init.lua
+++ init.lua
@@ -17,10 +17,10 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({ { import = "plugins" }, { import = "plugins.lsp" } }, {
-	checker = {
-		enabled = true,
-		notify = false,
-	},
+	-- checker = {
+	-- 	enabled = true,
+	-- 	notify = false,
+	-- },
 	change_detection = {
 		notify = false,
 	},
