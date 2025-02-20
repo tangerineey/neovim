6beda5574e2d1bbfe59e7defcf13f1422563ed57
Ayaad Sharif <ayaad.contact@gmail.com>
2025-02-19 20:56:23 -0700

    remove lsp stuff

--- init.lua
+++ init.lua
@@ -16,7 +16,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "plugins" }, { import = "plugins.lsp" } }, {
+require("lazy").setup({ { import = "plugins" }, }, {
 	-- checker = {
 	-- 	enabled = true,
 	-- 	notify = false,
