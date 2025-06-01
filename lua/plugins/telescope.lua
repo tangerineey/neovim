cc5b8ec2f1fd7aec2cf36976bbe22784bb9125fb
Ayaad <ayaad.contact@gmail.com>
2025-06-01 16:30:25 -0600

    add telescope tag searching

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -34,7 +34,8 @@ return {
 		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
 		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+		keymap.set("n", "<leader>fT", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 		keymap.set("n", "<leader>fd", "<cmd>Telescope lsp_document_symbols<cr>", { desc = "Find function definitions and variables" })
+        keymap.set("n", "<leader>ft", "<cmd>Telescope tags<cr>", { desc = "Search tags" })
 	end,
 }
