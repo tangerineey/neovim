ba575474c98ef69c49d8058403815bcd2a35b0be
Ayaad <ayaad.contact@gmail.com>
2025-06-03 19:31:55 -0600

    telescope: after <C-q> open quickfix list and jump to first entry

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -19,7 +19,14 @@ return {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
 						["<C-j>"] = actions.move_selection_next, -- move to next result
 						-- Send ethier selected or all results from telescope to quickfix list 
-						["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+                        ["<C-q>"] = function(prompt_bufnr)
+                            local action_state = require("telescope.actions.state")
+                            local actions = require("telescope.actions")
+
+                            actions.smart_send_to_qflist(prompt_bufnr)
+                            actions.open_qflist(prompt_bufnr)
+                            vim.cmd("cfirst")
+                        end,
 					},
 				},
 			},
