4be6abfa5bdf481a1f810ac0f5d1b68385f0ac89
Ayaad <ayaad.contact@gmail.com>
2025-06-19 12:50:18 -0600

    treesitter: fix ruby indenting with methods

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -16,3 +16,14 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt_local.shiftwidth = 2
 	end,
 })
+
+-- In Ruby, methods chained with "." before "end" autoindent wrong as described
+-- here: https://github.com/tree-sitter/tree-sitter-ruby/issues/230 This is not
+-- a bug within treesitter so to get expected behavior we have to tweak
+-- indenting a bit.
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "ruby",
+	callback = function()
+		vim.opt_local.indentkeys:remove(".")
+	end,
+})
