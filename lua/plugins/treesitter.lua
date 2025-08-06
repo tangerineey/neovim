ddbbc9518d6c6b41b6d55743bbb15de587b25f49
Ayaad <ayaad.contact@gmail.com>
2025-08-05 19:10:00 -0600

    plugins: add nvim-treesitter-textobjects for more movements

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,6 +3,7 @@ return {
 	build = ":TSUpdate",
 	dependencies = {
 		"windwp/nvim-ts-autotag",
+        "nvim-treesitter/nvim-treesitter-textobjects",
 	},
 	config = function()
 		-- import nvim-treesitter plugin
