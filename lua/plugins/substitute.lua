a0a94786255297d1929ca0f159423ebf99b61e3f
Ayaad <ayaad.contact@gmail.com>
2025-08-06 11:57:54 -0600

    plugins: add flash plugin!

--- lua/plugins/substitute.lua
+++ lua/plugins/substitute.lua
@@ -1,17 +1,14 @@
 return {
-  "gbprod/substitute.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local substitute = require("substitute")
+    "gbprod/substitute.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local substitute = require("substitute")
 
-    substitute.setup()
+        substitute.setup()
 
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
-
-    vim.keymap.set("n", "s", substitute.operator, { desc = "Substitute with motion" })
-    vim.keymap.set("n", "ss", substitute.line, { desc = "Substitute line" })
-    vim.keymap.set("n", "S", substitute.eol, { desc = "Substitute to end of line" })
-    vim.keymap.set("x", "s", substitute.visual, { desc = "Substitute in visual mode" })
-  end,
+        vim.keymap.set("n", "<leader>r", substitute.operator, { noremap = true, desc = "Substitute with motion" })
+        vim.keymap.set("n", "<leader>rr", substitute.line, { noremap = true, desc = "Substitute line" })
+        vim.keymap.set("n", "<leader>R", substitute.eol, { noremap = true, desc = "Substitute to end of line" })
+        vim.keymap.set("x", "<leader>r", substitute.visual, { noremap = true, desc = "Substitute in visual mode" })
+    end,
 }
