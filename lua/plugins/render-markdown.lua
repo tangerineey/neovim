b321a9f536eef4a28883352ed8f688cc44b240c3
Ayaad <ayaad.contact@gmail.com>
2025-09-05 00:08:09 -0600

    plugins: disable latex for render-markdown

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,5 +1,9 @@
 return {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
-    opts = {},
+    opts = {
+        latex = {
+            enabled = false,
+        }
+    },
 }
